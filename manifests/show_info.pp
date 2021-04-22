1a166fae1f175f85e28bb2fcceff80f26baa2c66
Ivan Roseland <icroseland@gmail.com>
2021-04-22 13:22:55 -0500

    adding proof of eyaml

--- manifests/show_info.pp
+++ manifests/show_info.pp
@@ -1,5 +1,8 @@
 #
-class utilities::show_info(){
+class utilities::show_info(
+  $eyaml_proof = "failed"
+){
 notify {"Running with facts.puppet_type ${::puppet_type} fact defined":}
 notify {"The master used this time is ${::puppet_server} ":}
+notify {"eyaml test ${eyaml_proof} ":}
 }
