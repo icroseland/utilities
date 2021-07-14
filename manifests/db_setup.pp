60fff84f15105535f02a1053769d94fedb57be13
Ivan Roseland <icroseland@gmail.com>
2021-07-14 17:53:40 -0500

    lamer

--- manifests/db_setup.pp
+++ manifests/db_setup.pp
@@ -5,19 +5,23 @@ $puppetdb_server = $::fqdn,
 $postgresql_server = $::fqdn
 ){
 if ($puppetdb_server == $::fqdn) and ($postgresql_server == $::fqdn ){
-  class { 'puppetdb': }
+  class { 'puppetdb':
+  manage_firewall => false,
+  }
 }
 if ($puppetdb_server != $::fqdn) and ($postgresql_server == $::fqdn ){
   class { 'puppetdb::database::postgresql':
     listen_addresses => $postgres_server,
     postgresql_ssl_on => true,
-    puppetdb_server => $puppetdb_server
+    puppetdb_server => $puppetdb_server,
+    manage_firewall => false,
     }
 }
 if ($puppetdb_server == $::fqdn) and ($postgresql_server =! $::fqdn ){
   class { 'puppetdb::server':
     database_host => $postgres_server,
-    postgresql_ssl_on => true
+    postgresql_ssl_on => true,
+    manage_firewall => false,
     }
 }
 }
