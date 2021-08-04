4c11b103823955af161256db941b25f84cfcda3f
Ivan Roseland <icroseland@gmail.com>
2021-08-04 12:59:15 -0500

    lame

--- manifests/db_setup.pp
+++ manifests/db_setup.pp
@@ -6,22 +6,22 @@ $postgresql_server = $::fqdn
 ){
 if ($puppetdb_server == $::fqdn) and ($postgresql_server == $::fqdn ){
   class { 'puppetdb':
-  manage_firewall => false,
+    manage_firewall => false,  
   }
 }
 if ($puppetdb_server != $::fqdn) and ($postgresql_server == $::fqdn ){
   class { 'puppetdb::database::postgresql':
-    listen_addresses => $postgres_server,
+    listen_addresses  => $postgres_server,
     postgresql_ssl_on => true,
-    puppetdb_server => $puppetdb_server,
-    manage_firewall => false,
+    puppetdb_server   => $puppetdb_server,
+    manage_firewall   => false,
     }
 }
-if ($puppetdb_server == $::fqdn) and ($postgresql_server != $::fqdn ){
+if ($puppetdb_server == $::fqdn) and ($postgresql_server =! $::fqdn ){
   class { 'puppetdb::server':
-    database_host => $postgres_server,
+    database_host     => $postgres_server,
     postgresql_ssl_on => true,
-    manage_firewall => false,
+    manage_firewall   => false,    
     }
 }
 }
---
+++ manifests/fileshare.pp
@@ -0,0 +1,75 @@
+# build a fileshare host.
+class utilities::fileshare(
+  $vsftpd_user_conf = undef,
+  $my_cert = undef,
+  $pam_users = undef,
+  $site_name = undef,
+  $listen_port = undef,
+  $access_log = undef,
+  $error_log = undef,
+  $ssl = undef,
+  $ssl_certificate = undef,
+  $ssl_certificate_key = undef,
+  $locations_hash = undef
+){
+#check if we have a cert
+$my_cert_check = find_file($my_cert)
+if $my_cert_check {
+  notify {"No cert found at ${my_cert}, load the cert manually and then re-run puppet":}
+  if $my_cert == undef {
+    notify {'The Cert is not defined in hiera':}
+  }
+  } else {
+  include vsftpd
+
+  file { '/etc/vsftpd/vsftpd_user_conf':
+    ensure => 'directory',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+    }
+  $vsftpd_user_conf.each |String $user_conf|{
+    file {"/etc/vsftpd/vsftpd_user_conf/${user_conf}":
+      ensure  => file,
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+      content => 'local_root=/data/ftp/',
+      }
+      }
+  # we need to be kind of hardcore and use the nginx from nginx instead of redhats..
+  package { 'yum-utils':
+    ensure => 'installed'
+    }->
+  file { '/etc/yum.repos.d/nginx.repo':
+    ensure => 'file',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+    source => 'puppet:///modules/utilities/etc/yum.repos.d/nginx.repo',
+    }->
+  include nginx
+  }
+# setup the nginx allowed user files for pam.d
+file { '/etc/nginx/pam_users':
+  ensure => 'directory',
+  owner  => 'root',
+  group  => 'root',
+  mode   => '0644'
+  }
+$pam_users.each |String $pam_user| {
+  file {"/etc/nginx/pam_users/${pam_user}.allowed":
+    ensure  => 'file',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => $pam_user,
+    require => File['/etc/nginx/pam_users']
+    }
+  }
+package { 'libnginx-mod-http-auth-pam':
+  ensure => 'present',
+  notify => Service["nginx::service::${::nginx::service_name}"]
+  }
+
+}
